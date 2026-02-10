128676256930d121e60d0108535bdd03849bd141
miszc <miszczykmiloszk@gmail.com>
2026-02-10 18:31:49 +0100

    Fixed week 12

--- src/week12/algorithm.kt
+++ src/week12/algorithm.kt
@@ -2,15 +2,15 @@ package week12
 
 class RegularBox(val size: Int)
 
-data class DateBox(val size: Int)
+data class DataBox(val size: Int)
 
 
 fun main(){
     val r1 = RegularBox(10)
     val r2 = RegularBox(10)
 
-    val d1 = DateBox(10)
-    val d2 = DateBox(10)
+    val d1 = DataBox(10)
+    val d2 = DataBox(10)
 
     println("Regular comparison: ${r1 == r2}")
     println("Data comparison: ${d1 == d2}")
